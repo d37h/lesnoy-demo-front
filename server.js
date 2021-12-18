eaff7a8ed110e2377e439f975bb39680ba2dc2d9
a.strukov <a.strukov@tinkoff.ru>
2021-12-18 20:07:17 +0300

    fix deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var express = require('express');
 
 // App
 var app = express();
-app.set('port', (process.env.PORT || 4200));
+app.set('port', (process.env.PORT || 8080));
 
 // your angular-project folder
-app.use('/', express.static(__dirname + '/lesnoy_demo'));
+app.use('/', express.static(__dirname + '/dist/lesnoy-demo'));
 
 app.listen(app.get('port'), function() {
   console.log("running: port", app.get('port'));
